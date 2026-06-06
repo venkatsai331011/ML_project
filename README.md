74c77ef9f208d71d31d7f161f0c4c24e479c65bc
venkatsai331011 <venkatsaiteja033@gmail.comm>
2026-06-06 16:03:27 +0530

    Added project structure and setup files

---
+++ README.md
@@ -0,0 +1,10 @@
+# ML Project
+
+## Overview
+
+This project demonstrates an end-to-end machine learning pipeline.
+
+## Installation
+
+```bash
+pip install -r requirements.txt
---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='mlproject',
+    version='0.0.1',
+    author='venkat sai',
+    author_email='ee23b107@smail.iitm.ac.in',
+    packages=find_packages(),
+    install_requires=['pandas', 'numpy', 'seaborn']
+)
